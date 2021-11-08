461a4f4a036cbc5dab9d1d15961f6d4be2a4190d
ɯ̹t͡ɕʲi <u@chimata.org>
2021-11-09 02:48:28 +0900

    Finished Gatsby Tutorial Part 6

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const ArticlePage = ({ data }) => {
@@ -10,12 +9,11 @@ const ArticlePage = ({ data }) => {
       {
         data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/article/${node.slug}`}>{node.frontmatter.title}</Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
-            </article>
+          </article>
         ))
       }
       </ul>
@@ -32,7 +30,6 @@ export const query = graphql`
           date(formatString: "MMMM DD, YYYY")
         }
         id
-        body
         slug
       }
     }
